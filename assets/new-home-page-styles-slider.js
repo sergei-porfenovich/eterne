3bc1cc9b6873ae63b97e952ad20701c8a7885785
Artur <artur.chaikouski@smartapptech.net>
2022-06-27 14:13:48 +0300

    bugfix styles-slider

--- assets/new-home-page-styles-slider.js
+++ assets/new-home-page-styles-slider.js
@@ -1,12 +1,10 @@
 var swiperTwo = new Swiper("#styles-slider", {
-    slidesPerView: 'auto',
-    spaceBetween: 150,
+    slidesPerView: 1,
+    spaceBetween: 0,
     watchSlidesVisibility: true,
     centeredSlides: true,
-    simulateTouch : true,
     initialSlide: 1, 
     loop: true,
-    loopFillGroupWithBlank: false,
     navigation: {
         nextEl: ".styles-slider-next",
         prevEl: ".styles-slider-prev",
