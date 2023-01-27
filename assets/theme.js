c361107dd96e34f88ec8a83f9a6fa2a96742531d
artur-chaikouski <artur.chaikouski@smartapptech.net>
2023-01-27 12:01:35 +0300

    bug fix: sold out product

--- assets/theme.js
+++ assets/theme.js
@@ -2859,7 +2859,6 @@ $( document ).ready(function() {
     if (window.MutationObserver && $addToCartForm.length) {
       var config = { childList: true, subtree: true };
       product_variants_removed.forEach(function(item){
-        console.log(item)
         $('.single-option-selector option').filter(function() { return $(this).text() === item; }).attr('sold-out', true);
       });
     }
